48f09c01561ba8eb57ce13fd443cae2e43ecd198
Timmy Garrabrant <timmy.garrabrant@gmail.com>
2018-08-08 08:53:51 -0700

    Fix components

--- storybook/components.js
+++ storybook/components.js
@@ -1,4 +1,6 @@
 export { default as ThemeProvider } from './theme';
 export { default as Button } from './stories/Button/Button';
 export { default as Text } from './stories/Text/Text';
+export { default as Input } from './stories/Input/Input';
+export { default as Divider } from './stories/Divider/Divider';
 export { default as Touchable } from './stories/Touchable/Touchable';
